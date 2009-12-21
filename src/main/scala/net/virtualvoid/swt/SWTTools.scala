f9da8a04257a074c3f30c1ed4144537e34f3f166
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-12-21 19:16:07 +0100

    infoBox + textBox

--- src/main/scala/net/virtualvoid/swt/SWTTools.scala
+++ src/main/scala/net/virtualvoid/swt/SWTTools.scala
@@ -1,7 +1,10 @@
 package net.virtualvoid.swt
 
 object SWTTools {
-	import _root_.org.eclipse.swt.widgets.Widget
+	import _root_.org.eclipse.swt
+	import swt.SWT
+	import swt.widgets.{Widget, Display, MessageBox, Shell}
+	
 	case class ItemStorage[T](label: String){
 		def apply(w: Widget): Option[T] = Option(w.getData(label).asInstanceOf[T])
 		def update(w: Widget, data: T): Unit = w.setData(label, data)
@@ -14,5 +17,27 @@ object SWTTools {
 	implicit def widgetIsStorage(w: Widget): WithStorage = new WithStorage {
 		def apply[T](store: ItemStorage[T]): Option[T] = store(w)
 		def update[T](store: ItemStorage[T], data: T): Unit = store(w) = data
+	}
+	
+	def infoBox(str: String): Unit = {
+		val box = new MessageBox(Display.getDefault.getActiveShell, SWT.ICON_INFORMATION)
+		box.setMessage(str)
+		box.open
+	}
+	
+	def textBox(str: String): Unit = {
+		import swt.layout.FillLayout
+		import swt.widgets.Text
+		import swt.graphics.Font
+		
+		val box = new Shell(Display.getCurrent, SWT.DIALOG_TRIM | SWT.RESIZE);
+		box.setLayout(new FillLayout)
+		
+		val edit = new Text(box, SWT.MULTI | SWT.V_SCROLL)
+		edit.setText(str)
+		edit.setEditable(false)
+		edit.setFont(new Font(Display.getCurrent,"mono" ,8 ,SWT.NORMAL))
+		
+		box.open
 	}
 }
