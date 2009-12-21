27dc07c8a48cf346016b9c519fd90574b28cb4ae
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-12-21 19:16:50 +0100

    pdf browser

---
+++ src/main/scala/net/virtualvoid/pdf/PdfBrowser.scala
@@ -0,0 +1,111 @@
+package net.virtualvoid.pdf
+
+import _root_.net.virtualvoid.scala.Tools._
+import _root_.com.lowagie.text.pdf._
+import _root_.net.virtualvoid.swt.SWTTools._
+
+object IOTools {
+	import _root_.java.io._
+	
+	def hexdumpd(data: Array[Byte]): String = new String(pipeThrough("hexdump -C", data))
+	
+	def pipeThrough(cmd: String, data: Array[Byte]): Array[Byte] = {
+		val baos = new ByteArrayOutputStream
+		val proc = Runtime.getRuntime.exec(cmd)
+		
+		val os = proc.getOutputStream()
+		val is = proc.getInputStream()
+		os.write(data)
+		os.close()
+		
+		var read = 0
+		val buffer = new Array[Byte](100)
+		
+		do {
+			read = is.read(buffer)
+			if (read >= 0)
+				baos.write(buffer,0,read)
+		}while (read >= 0)
+			
+		baos.toByteArray()
+	}
+}
+
+object PdfBrowser extends net.virtualvoid.swt.TreeApp[PdfReader] {
+	val reader = new PdfReader("test.pdf")
+	
+	import PdfObject._
+	def typeStr(tpe: Int): String = tpe match {
+		case ARRAY => "ARRAY"
+	    case BOOLEAN => "BOOLEAN"
+	    case DICTIONARY => "DICTIONARY"
+	    case NAME => "NAME"
+	    case NUMBER => "NUMBER"
+	    case STRING => "STRING"
+	    case INDIRECT => "INDIRECT"
+	    case STREAM => "STREAM"
+	    case _ => tpe.toString
+	}
+	def typeStr(obj: PdfObject): String = obj.`type` match {
+		case INDIRECT => "INDIRECT => "+typeStr(PdfReader.getPdfObject(obj).`type`)
+		case x => typeStr(x)
+	}
+	
+	val startObject: PdfReader = reader
+	
+	import net.virtualvoid.swt.Trees._
+	
+	def asSeq[T](col: java.util.Collection[_]): Seq[T] = col.toArray.toSeq.asInstanceOf[Seq[T]]
+	
+	def dictionaryEntries(dic: PdfDictionary): Iterable[(String, PdfObject)] =
+		asSeq[PdfName](dic.getKeys) map (name => (name.toString, dic.get(name)))
+		
+	def arrayEntries(arr: PdfArray): Iterable[PdfObject] =
+		asSeq[PdfObject](arr.getArrayList())
+	
+	def pdfObject: ItemInfo[PdfObject] =
+		choose {
+		case p:PdfString => string
+		case p:PdfIndirectReference => indirect
+		case p:PdfName => name
+		case p:PdfArray => array
+		case p:PdfNumber => number
+		case p:PRStream => stream
+		case p:PdfDictionary => dictionary
+		}
+	
+	def stream: ItemCreator[PRStream] =
+		item[PRStream].|-!("Length: "+_.getRawLength)
+					   .|--(dictionary labelled "Dictionary")
+					   .dblClick(x => textBox(IOTools.hexdumpd(PdfReader.getStreamBytes(x))))
+					   
+	
+	def array: ItemCreator[PdfArray] =
+		item[PdfArray].|-*(arrayEntries(_).zipWithIndex) { info =>
+			info.as(_._1)(pdfObject).labelled(x => "["+x._2.toString+"]: "+typeStr(x._1))
+		}
+	
+	def indirect: ItemCreator[PdfIndirectReference] =
+		item[PdfIndirectReference]//.|-!("<indirect> "+_)
+		                          .as(PdfReader.getPdfObject(_))(pdfObject)
+	
+    def name: ItemCreator[PdfName] = 
+    	item[PdfName] |-! ("<name> "+_.toString)
+    	
+    def number: ItemCreator[PdfNumber] = 
+    	item[PdfNumber] |-! (_.toString)
+    	
+	def string: ItemCreator[PdfString] =
+		item[PdfString] |-! ("'"+_.toString+"'")
+		
+	def dictionary: ItemInfo[PdfDictionary] = 
+		item[PdfDictionary].|-*(dictionaryEntries) { info =>
+		  	info.as(_._2)(pdfObject).labelled(x => x._1+": "+typeStr(x._2))
+		}
+	
+	def treeCreator: ItemCreator[PdfReader] = 
+		item[PdfReader].labelled(_.toString)
+		               .|--(_.getCatalog)(dictionary labelled "Catalog")
+	
+	def main(args: Array[String]) { run }
+}
