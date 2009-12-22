ea12128a4a30947df44052c28fa6caf6d37619a5
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-12-22 12:15:43 +0100

    more compact tree rows in GTK

--- src/main/scala/net/virtualvoid/swt/Tree.scala
+++ src/main/scala/net/virtualvoid/swt/Tree.scala
@@ -100,5 +100,19 @@ object Trees {
 	def register(t: Tree) {
 		t.addTreeListener(TreeListener)
 		t.addMouseListener(TreeListener)
+		
+		if (SWT.getPlatform() == "gtk")
+			GTK.tightCells(t)
+	}
+}
+
+object GTK {
+	def tightCells(t: Tree) {
+		import swt.internal.gtk.OS
+		val rendererMethod = t.getClass.getDeclaredMethod("getTextRenderer", java.lang.Long.TYPE)
+		rendererMethod.setAccessible(true)
+		val h = OS.gtk_tree_view_get_column (t.handle, 0)
+		val r = rendererMethod.invoke(t,java.lang.Long.valueOf(h)).asInstanceOf[java.lang.Long].longValue
+		OS.g_object_set (r, OS.ypad, 0, 0)		
 	}
 }
